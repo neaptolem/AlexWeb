75180ce7358d49fc19fc596d39d378b90d23831c
andrian <Anneaptolemkob@gmail.com>
2016-10-19 10:20:46 +0300

    add changes to navbar design

--- src/js/index.js
+++ src/js/index.js
@@ -1,38 +1,4 @@
-// Google Maps Scripts
-var map = null;
-var width=12.4101671;
-var height=51.3263266;
-google.maps.event.addDomListener(window, 'load', init);
-google.maps.event.addDomListener(window, 'resize', function() {
-    map.setCenter(new google.maps.LatLng(height, width));
-});
-
-function init() {
-    var mapOptions = {
-        zoom: 16,
-        center: new google.maps.LatLng(height, width), 
-
-        disableDefaultUI: true,
-        scrollwheel: false,
-        draggable: false,
-        styles:[]
-    };
-
-
-    var mapElement = document.getElementById('map');
-
-    map = new google.maps.Map(mapElement, mapOptions);
-
-   var image = 'img/marker.png';
-    var myLatLng = new google.maps.LatLng(51.326297,12.4109433);
-    var beachMarker = new google.maps.Marker({
-        position: myLatLng,
-        map: map,
-        icon: image
-    });
-}
-
-
+(function(){
 $('.navbar-collapse ul li a').click(function() {
     if($(document).width()<=990){
     $(this).closest('.collapse').collapse('toggle');
@@ -51,10 +17,10 @@ $(function() {
 });
 
 $("#navbar a").on("click", function(){
-   $(".nav").find(".active").removeClass("active");
+   $("#navbar").find(".active").removeClass("active");
    $(this).parent().addClass("active");
 });
-
+console.log('error');
 var modal = document.getElementById('ImpressumModal');
 
 var btn = document.getElementById("impressumBtn");
@@ -73,4 +39,39 @@ window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
+}
+})();
+
+// Google Maps Scripts
+var map = null;
+var width=12.4101671;
+var height=51.3263266;
+google.maps.event.addDomListener(window, 'load', init);
+google.maps.event.addDomListener(window, 'resize', function() {
+    map.setCenter(new google.maps.LatLng(height, width));
+});
+
+function init() {
+    var mapOptions = {
+        zoom: 16,
+        center: new google.maps.LatLng(height, width),
+
+        disableDefaultUI: true,
+        scrollwheel: false,
+        draggable: false,
+        styles:[]
+    };
+
+
+    var mapElement = document.getElementById('map');
+
+  map = new google.maps.Map(mapElement, mapOptions);
+
+   var image = 'img/marker.png';
+    var myLatLng = new google.maps.LatLng(51.326297,12.4109433);
+    var beachMarker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        icon: image
+    });
 }
